cc33fa15f3346a23569a726b516f73424740976b
enniwealth <wealththefirstx1000@gmail.com>
2024-06-14 16:29:14 +0100

    change destination blockchain ID

---
+++ send-and-receive/senderOnCChain.sol
@@ -0,0 +1,29 @@
+// (c) 2023, Ava Labs, Inc. All rights reserved.
+// See the file LICENSE for licensing terms.
+
+// SPDX-License-Identifier: Ecosystem
+
+pragma solidity ^0.8.18;
+
+import "@teleporter/ITeleporterMessenger.sol";
+
+contract SenderOnCChain {
+    ITeleporterMessenger public immutable messenger = ITeleporterMessenger(0x253b2784c75e510dD0fF1da844684a1aC0aa5fcf);
+
+    /**
+     * @dev Sends a message to another chain.
+     */
+    function sendMessage(address destinationAddress, string calldata message) external {
+        messenger.sendCrossChainMessage(
+            TeleporterMessageInput({
+                // Replace with blockchainID of your Subnet (see instructions in Readme)
+                destinationBlockchainID: 0x439e2bacce79a9ef60948b4258e41166ec76e15e20a119acbd80cfd57958fd7c,
+                destinationAddress: destinationAddress,
+                feeInfo: TeleporterFeeInfo({feeTokenAddress: address(0), amount: 0}),
+                requiredGasLimit: 100000,
+                allowedRelayerAddresses: new address[](0),
+                message: abi.encode(message)
+            })
+        );
+    }
+}
